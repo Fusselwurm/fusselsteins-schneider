7e9451f3739e86e1e3da1f19538fc268c3bd4773
Moritz Schmidt <fusselwurm@gmail.com>
2019-02-21 18:32:06 +0100

    update paths

--- do_ffmpeg.bat
+++ do_ffmpeg.bat
@@ -8,8 +8,8 @@ if not defined startAt set startAt=00:00:00
 @set duration=%3
 if not defined duration set duration="02:00:00"
 
-@set ffmpeg=C:\Program Files\ffmpeg\bin\ffmpeg.exe
-@set sox=C:\Program Files (x86)\sox-14-4-1\sox.exe
+@set ffmpeg=C:\Program Files\ffmpeg-20190219-ff03418-win64-static\bin\ffmpeg.exe
+@set sox=C:\Program Files (x86)\sox-14-4-2\sox.exe
 
 "%ffmpeg%" ^
   -ss %startAt% ^
